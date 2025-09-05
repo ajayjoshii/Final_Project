c7fda399b79a90f5d5a8f7587f4e9fc40e3504b7
ajayjoshii <jajay7941@gmail.com>
2025-09-05 18:45:49 +0545

    ajay

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -27,21 +27,22 @@ connectToMongo();
 
 const app = express();
 
-// const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-// }));
-
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
 app.use(cors({
-  origin: '*'
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
+
+
+// app.use(cors({
+//   origin: '*'
+// }));
 
 
 app.use(bodyParser.json());
